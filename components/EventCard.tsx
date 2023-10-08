6133db2df997e3bb7393724c4ff6400e10286035
Chase McDougall <chasemcdougall@hotmail.com>
2023-10-08 15:51:13 -0400

    refactor eventcard

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -15,50 +15,43 @@ export interface EventCardProps {
 
 export default function EventCard(props: EventCardProps) {
   return (
-    <div className="flex max-w-sm flex-col gap-2 rounded-lg border border-gray-200 bg-white shadow">
+    <div className="flex max-w-sm flex-col gap-1 rounded-lg border border-gray-200 bg-white p-3 shadow">
       {/* <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" /> */}
-      <div className="flex flex-row px-3">
-        <div className="flex flex-col gap-1">
-          <h5 className="text-lg font-bold text-gray-900 ">
-            {props.eventName}
-          </h5>
-          <div className="flex flex-row gap-1">
-            <MapPinIcon
-              className="h-5 w-5 stroke-1 text-black"
-              aria-hidden="true"
-            />
-            <p className="text-sm font-normal text-gray-700">
-              {props.eventLocation}
-            </p>
-          </div>
+
+      <div className="flex flex-row items-center justify-between">
+        <h5 className="text-lg font-bold text-gray-900 ">{props.eventName}</h5>
+        <div className="flex flex-row-reverse gap-1">
+          <CalendarDaysIcon
+            className="h-5 w-5 stroke-1 text-black"
+            aria-hidden="true"
+          />
+          <p className="text-sm font-normal text-gray-700">{props.eventDate}</p>
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-between">
+        <div className="flex flex-row gap-1">
+          <MapPinIcon
+            className="h-5 w-5 stroke-1 text-black"
+            aria-hidden="true"
+          />
           <p className="text-sm font-normal text-gray-700">
-            {props.eventDescription}
+            {props.eventLocation}
           </p>
         </div>
-
-        <div className="flex flex-col gap-2">
-          <div className="flex flex-row-reverse gap-1">
-            <CalendarDaysIcon
-              className="h-5 w-5 stroke-1 text-black"
-              aria-hidden="true"
-            />
-            <p className="text-sm font-normal text-gray-700">
-              {props.eventDate}
-            </p>
-          </div>
-          <div className="flex flex-row-reverse gap-1">
-            <ClockIcon
-              className="h-5 w-5 stroke-1 text-black"
-              aria-hidden="true"
-            />
-            <p className="text-sm font-normal text-gray-700">
-              {props.eventTime}
-            </p>
-          </div>
+        <div className="flex flex-row-reverse gap-1">
+          <ClockIcon
+            className="h-5 w-5 stroke-1 text-black"
+            aria-hidden="true"
+          />
+          <p className="text-sm font-normal text-gray-700">{props.eventTime}</p>
         </div>
       </div>
 
-      <div className="flex flex-row gap-1 px-3 pb-3">
+      <p className="text-sm font-normal text-gray-700">
+        {props.eventDescription}
+      </p>
+
+      <div className="flex flex-row gap-1">
         {props.eventTags.map((tag, id) => (
           <div key={id} className="rounded-full bg-gray-100 px-2">
             <p className="text-sm font-normal text-gray-700">{tag}</p>
