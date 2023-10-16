98bed6b71ac13120ff3c8bab844354aa5a323975
AsherHounsell <asher.w.hounsell@gmail.com>
2023-10-16 19:59:14 -0400

    Updating frontend

--- app/events/manage/page.tsx
+++ app/events/manage/page.tsx
@@ -18,59 +18,51 @@ export interface EventCardProps {
 export default function Home() {
   const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(true); // Initialize isOpen state
 
-  const onCloseDrawer = () => {
-    setIsDrawerOpen(false);
-  };
   const toggleDrawer = () => {
-    setIsDrawerOpen((prev) => !prev);
+    setIsDrawerOpen((prevState) => !prevState);
   };
+  
 
   return (
-    <div className="flex h-full flex-row">
+    <main className="relative h-screen w-full">
       <Drawer
         isOpen={isDrawerOpen}
-        style={isDrawerOpen ? '' : '-translate-x-full'}
-        onClose={onCloseDrawer}
+        style={`overflow-y-auto absolute h-screen bg-slate-50 left-0 transform-transition duration-500 w-1/3 bg-slate-50 ${
+          isDrawerOpen ? '' : '-translate-x-full'
+        }`}
+        onClose={toggleDrawer}
       >
-        <EventForm onClose={onCloseDrawer} />
+        <EventForm onClose={toggleDrawer}/>
       </Drawer>
       <div
-        className={`transform-transition absolute right-0 h-[calc(100vh-64px)] duration-500 ${
+        className={`transform-transition absolute right-0 h-screen duration-500 ${
           isDrawerOpen ? 'w-2/3 ' : 'w-full'
         }`}
       >
-        <div className="flex h-9 w-full items-center bg-slate-50 p-3">
-          <h5
-            className="cursor-pointer text-lg font-bold"
-            onClick={toggleDrawer}
-          >
-            Create Event
-          </h5>
+        <div onClick={toggleDrawer} className="cursor-pointer flex h-9 w-full items-center bg-slate-50 p-3">
+          <h5 className="text-lg font-bold">Create Event</h5>
           <SquaresPlusIcon
-            className="ml-2 h-5 w-5 cursor-pointer fill-current stroke-1 text-black"
+            className="ml-2 h-5 w-5 fill-current stroke-1 text-black"
             aria-hidden="true"
-            onClick={toggleDrawer}
           />
         </div>
-        <div className="grid h-[calc(100vh-64px-36px)] grid-cols-3 gap-4 overflow-y-auto p-4">
+        <div className="grid grid-cols-3 gap-4 p-4">
           {/* Create some dummy events */}
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].map(
-            (id) => (
-              <EventCard
-                key={id}
-                eventName={'Awesome Concert'}
-                eventDescription={
-                  'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
-                }
-                eventLocation={'Galbraith 202'}
-                eventDate={'29/10/2023'}
-                eventTime={'8-10 pm'}
-                eventTags={['Programming', 'Music', 'Dance']}
-              />
-            )
-          )}
+          {[1, 2, 3, 4, 5, 6, 7].map((id) => (
+            <EventCard
+              key={id}
+              eventName={'Awesome Concert'}
+              eventDescription={
+                'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
+              }
+              eventLocation={'Galbraith 202'}
+              eventDate={'29/10/2023'}
+              eventTime={'8-10 pm'}
+              eventTags={['Programming', 'Music', 'Dance']}
+            />
+          ))}
         </div>
       </div>
-    </div>
+    </main>
   );
 }
