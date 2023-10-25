3a59d65f09d01d2e013aec8955469be89cb9bfa7
Chase McDougall <chasemcdougall@hotmail.com>
2023-10-25 12:50:54 -0400

    fix(event feed): scroll

--- app/events/find/feed/page.tsx
+++ app/events/find/feed/page.tsx
@@ -2,22 +2,24 @@ import EventCard from '@/components/EventCard';
 
 export default function page() {
   return (
-    <div className="flex w-full flex-col items-center">
+    <div className="flex h-[calc(100vh-64px-64px)] w-full flex-col items-center overflow-y-auto">
       <div className="grid grid-cols-3 gap-4 p-4">
         {/* Create some dummy events */}
-        {[1, 2, 3, 4, 5, 6, 7].map((id) => (
-          <EventCard
-            key={id}
-            eventName={'Awesome Concert'}
-            eventDescription={
-              'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
-            }
-            eventLocation={'Galbraith 202'}
-            eventDate={'29/10/2023'}
-            eventTime={'8-10 pm'}
-            eventTags={['Programming', 'Music', 'Dance']}
-          />
-        ))}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].map(
+          (id) => (
+            <EventCard
+              key={id}
+              eventName={'Awesome Concert'}
+              eventDescription={
+                'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
+              }
+              eventLocation={'Galbraith 202'}
+              eventDate={'29/10/2023'}
+              eventTime={'8-10 pm'}
+              eventTags={['Programming', 'Music', 'Dance']}
+            />
+          )
+        )}
       </div>
     </div>
   );
