cbf56245bd9b995e5757f9057a76b7414ae42d94
Joshua Pow <jokesjosh@gmail.com>
2023-10-29 17:05:58 -0400

    added tests for map

---
+++ __tests__/mapBox.test.tsx
@@ -0,0 +1,43 @@
+import MapBox from '@/components/MapBox';
+import { act, render } from '@testing-library/react';
+import mapboxgl from 'mapbox-gl';
+
+// Mock the Mapbox library
+jest.mock('mapbox-gl', () => ({
+  Map: jest.fn(() => ({
+    on: jest.fn(),
+    addSource: jest.fn(),
+    setTerrain: jest.fn(),
+    addLayer: jest.fn()
+  }))
+}));
+
+describe('MapBox', () => {
+  const mockedMapboxglMap = mapboxgl.Map;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders without crashing', () => {
+    render(<MapBox />);
+  });
+
+  it('initializes the map', () => {
+    act(() => {
+      render(<MapBox />);
+    });
+
+    expect(mockedMapboxglMap).toHaveBeenCalledTimes(1);
+    expect(mockedMapboxglMap).toHaveBeenCalledWith(
+      expect.objectContaining({
+        accessToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
+        container: 'map',
+        style: 'mapbox://styles/mapbox/standard-beta',
+        center: [-79.39486600749379, 43.66027265761257],
+        zoom: 14,
+        pitch: 60
+      })
+    );
+  });
+});
