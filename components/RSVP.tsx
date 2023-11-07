070e88c0400c38cea6d75715299de7148b3dca24
TashanManiyalaghan <tashan.maniyalaghan@mail.utoronto.ca>
2023-11-06 20:39:53 -0500

    Initial commit for RSVP.

---
+++ components/RSVP.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// eslint-disable-next-line @typescript-eslint/ban-types
+type Props = {};
+
+const RSVP = (props: Props) => {
+  return (
+    <div>
+      <button className="btn">RSVP</button>
+    </div>
+  );
+};
+
+export default RSVP;
