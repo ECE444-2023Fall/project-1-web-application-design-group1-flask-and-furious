f58dad982db258ae5867d1523570b620c631e4f1
Joshua Pow <jokesjosh@gmail.com>
2023-11-06 22:00:57 -0500

    trying to fix evenForm test failing due to supabase mock not working

--- __tests__/eventForm.test.tsx
+++ __tests__/eventForm.test.tsx
@@ -1,6 +1,17 @@
 import EventForm, { formProps } from '@/components/EventForm';
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+
+jest.mock('@supabase/auth-helpers-nextjs', () => ({
+  createClientComponentClient: () => ({
+    from: () => ({
+      select: () =>
+        Promise.resolve({ data: [{ tag: 'Tag1' }, { tag: 'Tag2' }] })
+    })
+  })
+}));
+
+// Rest of your test code
 
 describe('EventForm', () => {
   const mockOnClose = jest.fn();
@@ -21,32 +32,36 @@ describe('EventForm', () => {
       tags: ['Tag 1', 'Tag 3']
     },
     Update: mockUpdate,
-    isNewEvent: false
+    isNewEvent: false,
+    Delete: function (): void {
+      throw new Error('Function not implemented.');
+    }
   };
 
   it('Render form with the appropriate initialFormData', async () => {
     render(<EventForm {...props} />);
 
-    // Check the basic information in the form:
-    expect(
-      screen.getByDisplayValue('Awesome Cool Concert')
-    ).toBeInTheDocument();
-    expect(
-      screen.getByDisplayValue(
-        'Super awesome concert that everyone want to go to!'
-      )
-    ).toBeInTheDocument();
-    expect(screen.getByDisplayValue('Perfect Location')).toBeInTheDocument();
-    expect(screen.getByDisplayValue('21:00:00')).toBeInTheDocument();
-    expect(screen.getByDisplayValue('23:00:00')).toBeInTheDocument();
-    expect(screen.getByDisplayValue('2023-10-28')).toBeInTheDocument();
-    expect(screen.getByDisplayValue('Weekly')).toBeInTheDocument();
+    await waitFor(() => {
+      expect(
+        screen.getByDisplayValue('Awesome Cool Concert')
+      ).toBeInTheDocument();
+      expect(
+        screen.getByDisplayValue(
+          'Super awesome concert that everyone want to go to!'
+        )
+      ).toBeInTheDocument();
+      expect(screen.getByDisplayValue('Perfect Location')).toBeInTheDocument();
+      expect(screen.getByDisplayValue('21:00:00')).toBeInTheDocument();
+      expect(screen.getByDisplayValue('23:00:00')).toBeInTheDocument();
+      expect(screen.getByDisplayValue('2023-10-28')).toBeInTheDocument();
+      expect(screen.getByDisplayValue('Weekly')).toBeInTheDocument();
 
-    // Check the selected tags:
-    const tagsSelect = screen.getByLabelText('Tags:') as HTMLSelectElement;
-    expect(tagsSelect.selectedOptions).toHaveLength(2);
-    expect(
-      Array.from(tagsSelect.selectedOptions).map((option) => option.value)
-    ).toEqual(['Tag 1', 'Tag 3']);
+      // Check the selected tags:
+      const tagsSelect = screen.getByLabelText('Tags:') as HTMLSelectElement;
+      expect(tagsSelect.selectedOptions).toHaveLength(2);
+      expect(
+        Array.from(tagsSelect.selectedOptions).map((option) => option.value)
+      ).toEqual(['Tag 1', 'Tag 3']);
+    });
   });
 });
--- __tests__/tags.test.tsx
+++ __tests__/tags.test.tsx
@@ -5,8 +5,20 @@ import { render, screen } from '@testing-library/react';
 // Tashan Maniyalaghan Tags Test Function
 
 describe('Tags', () => {
+  const mockSelectedTags = {
+    tag1: true,
+    tag2: false
+  };
+
+  const mockSetSelectedTags = jest.fn();
+
   const setup = () => {
-    const utils = render(<Tags />);
+    const utils = render(
+      <Tags
+        selectedTags={mockSelectedTags}
+        setSelectedTags={mockSetSelectedTags}
+      />
+    );
     const tagsDropdown = screen.getByTestId('tagsDropdown');
     return { tagsDropdown, ...utils };
   };
