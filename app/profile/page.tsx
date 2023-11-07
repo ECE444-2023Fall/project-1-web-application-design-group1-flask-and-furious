8c68bc282b2734fa081d70d64f41caa5788e8247
Ethan Zheng <ethanzheng11@gmail.com>
2023-11-07 01:38:08 -0500

    Fixed a useEffect error.

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -72,7 +72,8 @@ export default function Profile() {
 
   useEffect(() => {
     getProfile();
-  }, [getProfile]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const onEditClick = () => {
     setProfileEdit(true);
