e8e71034587f0d1d94bcf32432098df5dd008768
Chase McDougall <chasemcdougall@hotmail.com>
2023-11-08 18:03:11 -0500

    make useful tests

--- api/tests/test_events.py
+++ api/tests/test_events.py
@@ -2,6 +2,7 @@ from api.index import app
 import os
 import pytest
 from supabase import Client, create_client
+import json
 
 @pytest.fixture
 def client():
@@ -26,8 +27,24 @@ def client():
         yield client
         supabase.auth.sign_out()
 
+# Check that valid data is received from events endpoint
 def test_get_all_events(client):
     response = client.get("/api/events", follow_redirects=True)
+    event_data =json.loads(response.json)['data']
 
-    # print(response.data)
+    assert len(event_data) > 0
+    assert response.status_code == 200
+
+# Test that filtering results reduces the number of results
+def test_get_owned_events(client):
+    response = client.get("/api/events", follow_redirects=True)
+    event_data =json.loads(response.json)['data']
+
+    filtered_response = client.get(
+            "/api/events" + "?userUuid=5b79b99c-b5f8-4394-8b30-da22ebaca975", 
+            follow_redirects=True
+        )
+    filtered_event_data =json.loads(filtered_response.json)['data']
+
+    assert len(event_data) > len(filtered_event_data)
     assert response.status_code == 200
