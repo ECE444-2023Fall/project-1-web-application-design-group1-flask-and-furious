bcd4f7999f29175f210516b30132d0f76cbb7bac
Chase McDougall <chasemcdougall@hotmail.com>
2023-11-08 23:08:00 -0500

    put reduced logo in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,19 @@ function Navbar({ session }: Props) {
   return (
     <nav className="flex h-16 w-full flex-row justify-between border-b bg-white p-6 text-black">
       <Link href="/" className="flex flex-row items-center">
-        ClubHub
+        <div className="flex h-full flex-col items-center justify-center bg-violet-50">
+          <div className="group relative">
+            <div className="absolute -inset-0.5 animate-tilt rounded-lg bg-violet-600 bg-gradient-to-r from-pink-600 to-violet-600 opacity-75 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
+            <div className="relative rounded-lg bg-violet-100 px-2 py-1">
+              <h1 className="scroll-m-20 text-lg font-extrabold tracking-tighter">
+                Club
+                <span className="animate-text bg-gradient-to-r from-violet-600 to-pink-600 bg-clip-text px-1 text-transparent">
+                  Hub
+                </span>
+              </h1>
+            </div>
+          </div>
+        </div>
       </Link>
 
       <div className="flex items-center gap-4">
