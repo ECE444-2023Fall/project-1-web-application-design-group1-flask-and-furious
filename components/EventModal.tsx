3c74eb94685236bd0196ddafa1f23639b0309e88
Chase McDougall <chasemcdougall@hotmail.com>
2023-11-13 15:46:30 -0500

    add event details to modal

--- components/EventModal.tsx
+++ components/EventModal.tsx
@@ -1,11 +1,17 @@
+import { formatTime } from '@/app/events/helpers';
 import { EventData } from '@/app/events/types';
 import {
+  CalendarDaysIcon,
+  ClockIcon,
+  MapPinIcon
+} from '@heroicons/react/24/outline';
+import {
   Button,
+  Image,
   Modal,
   ModalBody,
   ModalContent,
-  ModalFooter,
-  ModalHeader
+  ModalFooter
 } from '@nextui-org/react';
 
 export interface EventModalProps {
@@ -17,33 +23,60 @@ export interface EventModalProps {
 export default function EventModal(props: EventModalProps) {
   const isOpen = props.isOpen;
   const onOpenChange = props.onOpenChange;
+  const event = props.event;
 
   return (
     <Modal backdrop="blur" isOpen={isOpen} onOpenChange={onOpenChange}>
       <ModalContent>
         {(onClose) => (
           <>
-            <ModalHeader className="flex flex-col gap-1">
-              {props.event.Title}
-            </ModalHeader>
+            <Image
+              width={450}
+              height={200}
+              className="rounded-t-lg"
+              src={event.image_url}
+              fallbackSrc="https://yqrgbzoauzaaznsztnwb.supabase.co/storage/v1/object/public/Images/no-image"
+            />
             <ModalBody>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                pulvinar risus non risus hendrerit venenatis. Pellentesque sit
-                amet hendrerit risus, sed porttitor quam.
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                pulvinar risus non risus hendrerit venenatis. Pellentesque sit
-                amet hendrerit risus, sed porttitor quam.
-              </p>
-              <p>
-                Magna exercitation reprehenderit magna aute tempor cupidatat
-                consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
-                incididunt cillum quis. Velit duis sit officia eiusmod Lorem
-                aliqua enim laboris do dolor eiusmod. Et mollit incididunt nisi
-                consectetur esse laborum eiusmod pariatur proident Lorem eiusmod
-                et. Culpa deserunt nostrud ad veniam.
+              <div className="flex flex-row items-center justify-between">
+                <h5 className="text-lg font-bold text-gray-900 ">
+                  {event.Title}
+                </h5>
+                <div className="flex flex-row-reverse gap-1">
+                  <CalendarDaysIcon
+                    className="h-5 w-5 stroke-1 text-black"
+                    aria-hidden="true"
+                  />
+                  <p className="text-sm font-normal text-gray-700">
+                    {event.Date}
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-row items-center justify-between">
+                <div className="flex flex-row gap-1">
+                  <MapPinIcon
+                    className="h-5 w-5 stroke-1 text-black"
+                    aria-hidden="true"
+                  />
+                  <p className="text-sm font-normal text-gray-700">
+                    {event.Location}
+                  </p>
+                </div>
+                <div className="flex flex-row-reverse gap-1">
+                  <ClockIcon
+                    className="h-5 w-5 stroke-1 text-black"
+                    aria-hidden="true"
+                  />
+                  <p className="text-sm font-normal text-gray-700">
+                    {`${formatTime(event.StartTime)} - ${formatTime(
+                      event.EndTime
+                    )}`}
+                  </p>
+                </div>
+              </div>
+
+              <p className="p-2 text-sm font-normal text-gray-700">
+                {event.Description}
               </p>
             </ModalBody>
             <ModalFooter>
