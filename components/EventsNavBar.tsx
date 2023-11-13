df2800773450fbc0306277db1cbec138e2bf8b7b
Chase McDougall <chasemcdougall@hotmail.com>
2023-11-13 17:21:10 -0500

    change feed bar from opacity adjustment to colour

--- components/EventsNavBar.tsx
+++ components/EventsNavBar.tsx
@@ -11,7 +11,7 @@ const FindEventsNavBar = () => {
           'flex flex-auto items-center justify-center shadow ' +
           (usePathname() === '/events/find/feed'
             ? 'bg-slate-200'
-            : 'bg-slate-50 hover:opacity-50')
+            : 'bg-slate-50 hover:bg-slate-200')
         }
       >
         Feed
@@ -22,7 +22,7 @@ const FindEventsNavBar = () => {
           'flex flex-auto items-center justify-center shadow ' +
           (usePathname() === '/events/find/map'
             ? 'bg-slate-200'
-            : 'bg-slate-50 hover:opacity-50')
+            : 'bg-slate-50 hover:bg-slate-200')
         }
       >
         Map
