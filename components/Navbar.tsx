9ceb92ec3ed4c78d484d857e2c29c25fdfc1caf5
Chase McDougall <chasemcdougall@hotmail.com>
2023-11-13 13:15:31 -0500

    fix dropdown link and remove old dropdown

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,90 +1,27 @@
 'use client';
+import {
+  Avatar,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Link,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem
+} from '@nextui-org/react';
 import { Session } from '@supabase/gotrue-js';
 import { usePathname } from 'next/navigation';
-import NavbarProfile from './NavbarProfile';
+import { useEffect, useState } from 'react';
 
 type Props = {
   session: Session | null;
 };
 
-function NavbarOld({ session }: Props) {
-  const pathname = usePathname();
-  return (
-    <nav className="flex h-16 w-full flex-row justify-between border-b bg-white p-6 text-black">
-      <Link href="/" className="flex flex-row items-center">
-        <div className="flex h-full flex-col items-center justify-center bg-violet-50">
-          <div className="group relative">
-            <div className="absolute -inset-0.5 animate-tilt rounded-lg bg-violet-600 bg-gradient-to-r from-pink-600 to-violet-600 opacity-75 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
-            <div className="relative rounded-lg bg-violet-100 px-2 py-1">
-              <h1 className="scroll-m-20 text-lg font-extrabold tracking-tighter">
-                Club
-                <span className="animate-text bg-gradient-to-r from-violet-600 to-pink-600 bg-clip-text px-1 text-transparent">
-                  Hub
-                </span>
-              </h1>
-            </div>
-          </div>
-        </div>
-      </Link>
-
-      <div className="flex items-center gap-4">
-        <Link
-          href="/events/find/feed"
-          className={
-            ['/events/find/feed', '/events/find/map'].includes(pathname)
-              ? 'font-bold underline-offset-auto'
-              : 'hover:opacity-50'
-          }
-        >
-          Find Events
-        </Link>
-        {session && (
-          <Link
-            href="/events/manage"
-            className={
-              pathname === '/events/manage'
-                ? 'font-bold underline-offset-auto'
-                : 'hover:opacity-50'
-            }
-          >
-            Manage Events
-          </Link>
-        )}
-        {session ? (
-          <NavbarProfile session={session} />
-        ) : (
-          <div className="m-2 flex items-center justify-center">
-            <Link
-              href="/login"
-              className={
-                'rounded-l-xl border bg-violet-600 px-4 py-1 text-white transition hover:bg-violet-500 ' +
-                (pathname === '/login' ? 'bg-violet-500' : '')
-              }
-            >
-              Sign In
-            </Link>
-            <Link
-              href="/register"
-              className={
-                'rounded-r-xl border bg-neutral-50 px-4 py-1 transition hover:bg-neutral-200 ' +
-                (pathname === '/register' ? 'bg-neutral-200' : '')
-              }
-            >
-              Sign Up
-            </Link>
-          </div>
-        )}
-      </div>
-    </nav>
-  );
-}
-
-import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react";
-import { useEffect, useState } from 'react';
-
 export default function Navigationbar({ session }: Props) {
   const [pictureUrl, setPictureUrl] = useState<string | null>(null);
-  
+
   const pathname = usePathname();
 
   useEffect(() => {
@@ -109,7 +46,7 @@ export default function Navigationbar({ session }: Props) {
   return (
     <Navbar>
       <NavbarBrand>
-        <Link href='/'>
+        <Link href="/">
           <div className="flex h-full flex-col items-center justify-center bg-violet-50">
             <div className="group relative">
               <div className="absolute -inset-0.5 animate-tilt rounded-lg bg-violet-600 bg-gradient-to-r from-pink-600 to-violet-600 opacity-75 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
@@ -126,22 +63,37 @@ export default function Navigationbar({ session }: Props) {
         </Link>
       </NavbarBrand>
 
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem isActive={['/events/find/feed', '/events/find/map'].includes(pathname)}>
-          <Link color={['/events/find/feed', '/events/find/map'].includes(pathname) ? "secondary" : "foreground"} href="/events/find/feed">
+      <NavbarContent className="hidden gap-4 sm:flex" justify="center">
+        <NavbarItem
+          isActive={['/events/find/feed', '/events/find/map'].includes(
+            pathname
+          )}
+        >
+          <Link
+            color={
+              ['/events/find/feed', '/events/find/map'].includes(pathname)
+                ? 'secondary'
+                : 'foreground'
+            }
+            href="/events/find/feed"
+          >
             Find Events
           </Link>
         </NavbarItem>
-        {session && <NavbarItem isActive={pathname === '/events/manage'}>
-          <Link color={pathname === '/events/manage' ? "secondary" : "foreground"} href="/events/manage">
-            Manage Events
-          </Link>
-        </NavbarItem>}
+        {session && (
+          <NavbarItem isActive={pathname === '/events/manage'}>
+            <Link
+              color={pathname === '/events/manage' ? 'secondary' : 'foreground'}
+              href="/events/manage"
+            >
+              Manage Events
+            </Link>
+          </NavbarItem>
+        )}
       </NavbarContent>
 
-
       <NavbarContent as="div" justify="end">
-        {session ?
+        {session ? (
           <Dropdown placement="bottom-end">
             <DropdownTrigger>
               <Avatar
@@ -151,7 +103,11 @@ export default function Navigationbar({ session }: Props) {
                 color="secondary"
                 name="Jason Hughes"
                 size="sm"
-                src={pictureUrl ? pictureUrl : "http://www.gravatar.com/avatar/?d=mp"}
+                src={
+                  pictureUrl
+                    ? pictureUrl
+                    : 'http://www.gravatar.com/avatar/?d=mp'
+                }
               />
             </DropdownTrigger>
             <DropdownMenu aria-label="Profile Actions" variant="flat">
@@ -159,22 +115,17 @@ export default function Navigationbar({ session }: Props) {
                 <p className="font-semibold">Signed in as</p>
                 <p className="font-semibold">{session.user.email}</p>
               </DropdownItem>
-              <DropdownItem key="settings">My Settings</DropdownItem>
-              <DropdownItem key="team_settings">Team Settings</DropdownItem>
-              <DropdownItem key="analytics">Analytics</DropdownItem>
-              <DropdownItem key="system">System</DropdownItem>
-              <DropdownItem key="configurations">Configurations</DropdownItem>
-              <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-              <DropdownItem key="logout" color="danger">
+              <DropdownItem href="profile">Profile</DropdownItem>
+              <DropdownItem color="danger">
                 <form action="/auth/sign-out" method="post">
-                  <button type='submit'>
+                  <button type="submit" className="flex w-full">
                     Log Out
                   </button>
                 </form>
               </DropdownItem>
             </DropdownMenu>
           </Dropdown>
-        :
+        ) : (
           <div className="m-2 flex items-center justify-center">
             <Link
               href="/login"
@@ -195,7 +146,7 @@ export default function Navigationbar({ session }: Props) {
               Sign Up
             </Link>
           </div>
-        }
+        )}
       </NavbarContent>
     </Navbar>
   );
